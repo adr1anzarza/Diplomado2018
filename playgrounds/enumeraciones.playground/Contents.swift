56e16c48e3579c10a355f2e561dbeef48f4bba84
Adrian Rojas Zarza <d182_adrian_z@Macbook-21.local>
2018-03-10 09:38:30 -0600

    enumeraciones added

---
+++ playgrounds/enumeraciones.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+enum  Dia {
+    case Lunes
+    case Martes
+    case Miércoles
+    case Jueves
+    case Viernes
+    case Sábado
+}
