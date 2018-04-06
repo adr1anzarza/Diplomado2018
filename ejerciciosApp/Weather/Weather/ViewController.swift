b204ac7f07b4e8e677e2bcd701b9386e72e88a91
Adrian Rojas Zarza <d182_adrian_z@Macbook-21.local>
2018-04-06 18:50:00 -0500

    Ejercicios added

--- ejerciciosApp/Weather/Weather/ViewController.swift
+++ ejerciciosApp/Weather/Weather/ViewController.swift
@@ -10,9 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let url = URL(string: "http://api.openweathermap.org/data/2.5/weather?q=London,uk&units=metric&APPID=b857156f364f34B82adae38d2d7efc60")
+        
+        let task = URLSession.shared.dataTask(
+            with: url!,
+            completionHandler:{ (data, response, error) in
+                if (data != nil){
+                    do{
+                        let json = try JSONSerialization.jsonObject(with: data!,options: []) as? [String: Any]
+                        DispatchQueue.main.sync {//Se hace así para que se puedan pintar los elementos visuales, ya que si no, no se pintan, deben ir en el thread principal
+                            if let main = json!["main"] as! [String: Any]? {
+                                self.temperatureLabel.text = "\(main["temp"] as! Float)º C"
+                            }
+                            self.locationLabel.text = json!["name"] as? String
+                        }
+                    
+                    } catch let e {
+                        print("Error retriving weather data: \(e)")
+                    }
+                }
+                
+        }
+        )
+        task.resume()
+        
     }
 
     override func didReceiveMemoryWarning() {
