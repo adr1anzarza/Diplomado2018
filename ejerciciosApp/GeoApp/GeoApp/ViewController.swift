a012e9b555e8bc653a7f466d0c5831c473ed6bd5
Adrian Rojas Zarza <d182_adrian_z@Macbook-21.local>
2018-06-15 16:21:37 -0500

    Geoapdd

--- ejerciciosApp/GeoApp/GeoApp/ViewController.swift
+++ ejerciciosApp/GeoApp/GeoApp/ViewController.swift
@@ -19,6 +19,34 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    let locationManager = CLLocationManager()
+    func enableLocationServices() {
+        locationManager.delegate = self
+        
+        switch CLLocationManager.authorizationStatus() {
+        case .notDetermined:
+            // Request when-in-use authorization initially
+            locationManager.requestWhenInUseAuthorization()
+            break
+            
+        case .restricted, .denied:
+            // Disable location features
+            disableMyLocationBasedFeatures()
+            break
+            
+        case .authorizedWhenInUse:
+            // Enable basic location features
+            enableMyWhenInUseFeatures()
+            break
+            
+        case .authorizedAlways:
+            // Enable any of your app's location features
+            enableMyAlwaysFeatures()
+            break
+        }
+    }
+}
 
 
 }
